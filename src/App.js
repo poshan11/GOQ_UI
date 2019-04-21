d09d241844f2d9249b6249a230dc6d9b3d33a4e1
poshan11 <ramji.poshan@gmail.com>
2019-04-21 13:54:59 -0700

    Adding src and html files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+
+   
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div className="Game-title"> Game Of Quests!</div>
+        </header>
+
+        {/* Google Maps */}
+       
+          </div>
+          );
+        }
+      }
+      
+      export default App;
